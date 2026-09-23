2b33052984ae637ef4366d17d5437d779fa7f69f
NgocYlvn <nguyenbich.ngoc@vn.yusen-logistics.com>
2026-09-23 14:08:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7373,7 +7373,7 @@ def main():
     segment_workload_table(f_workload, f_mode)
 
     
-    section_title("5. Workload Breakdown by Activity & Segment")
+    section_title("5. Workload Breakdown by Category & Segment")
 
     st.markdown(
         """
